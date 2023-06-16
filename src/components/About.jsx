22bac71e138d4e68380a98e244b94885abfc3958
shaikh salahuddin ahmed <125596394+salahuddin334@users.noreply.github.com>
2023-06-16 06:09:21 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,12 +37,7 @@ const About = () => {
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+       I am a skilled MERN (MongoDB, Express.js, React.js, Node.js) stack developer with 2 years of professional experience. With a strong foundation in web development, I have successfully designed and implemented full-stack solutions using the MERN stack
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
